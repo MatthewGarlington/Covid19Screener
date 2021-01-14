273544f3bba72a192962b9a90232f7983da8eeb6
Matthew Garlington <SBMatthewG@gmail.com>
2021-01-14 14:51:32 -0500

    Initial Commit

---
+++ Covid19Screener/Covid19ScreenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Covid19ScreenerApp.swift
+//  Covid19Screener
+//
+//  Created by Matthew Garlington on 1/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Covid19ScreenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
